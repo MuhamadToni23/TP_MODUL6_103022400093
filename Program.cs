7a492f11f6c3759efb2bcd6b141a47590b06290a
Muhamad Toni Kurniawan <muhammadtony898@gmail.com>
2026-04-05 18:04:07 +0700

    commit message

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using TP_MOD6_103022400093;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaMusicTrack lagu1 = new SayaMusicTrack("Lagu Santai");
+
+        // test normal
+        lagu1.IncreasePlayCount(5);
+
+        // test overflow (loop biar cepat)
+        for (int i = 0; i < 100; i++)
+        {
+            lagu1.IncreasePlayCount(int.MaxValue);
+        }
+
+        lagu1.PrintTrackDetails();
+    }
+}
---
+++ SayaMusicTrack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_MOD6_103022400093
+{
+    internal class SayaMusicTrack
+    {
+        private int id;
+        private int playCount;
+        private string title;
+        private static Random rand = new Random();
+        public SayaMusicTrack(string title)
+        {
+            Debug.Assert(title != null, "Title tidak boleh null");
+            Debug.Assert(title.Length <= 100, "Title maksimal 100 karakter");
+
+            this.title = title;
+            this.id = rand.Next(10000, 99999);
+            this.playCount = 0;
+        }
+        public void IncreasePlayCount(int count)
+        {
+            Debug.Assert(count <= 10000000, "Count terlalu besar");
+
+            try
+            {
+                checked
+                {
+                    playCount += count;
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Terjadi overflow!");
+            }
+        }
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine($"ID: {id}");
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Play Count: {playCount}");
+        }
+    }
+}
